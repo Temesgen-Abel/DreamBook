7cc6c004cd443ad91e8165d2159ffc1372f70223
Temesgen <abelneht@gmail.com>
2026-03-20 14:01:54 +0300

    update

--- server.js
+++ server.js
@@ -1066,6 +1066,7 @@ app.post("/create-group-meeting", mustBeLoggedIn, (req, res) => {
   return res.redirect(307, "/live-meetings/create");
 });
 
+
 // Main live meetings page - redirect to join
 app.get("/live-meetings", mustBeLoggedIn, (req, res) => {
   res.redirect("/live-meetings/join");
